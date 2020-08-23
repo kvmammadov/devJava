26764b8b7f8fa80b90d44f233a069d0cef8931d1
DevColibri <kamilmammadov853@gmail.com>
2020-08-23 10:09:10 +0400

    Adding OOP homeWork

--- firstLesson/src/main/java/OOP/Human.java
+++ firstLesson/src/main/java/OOP/Human.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package OOP;
 
-/**
- *
- * @author kvmammadov
- */
 public class Human {
+
+    String name;
+    String profession;
+    String sex;
+    int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    void print() {
+        System.out.println("Your name is: " + name); 
+        System.out.println("Your Age is: " + age); 
+        System.out.println("Your Profession is: " + profession); 
+        System.out.println("Your Sex is: " + sex);     
+    }
+
+    public static void main(String[] args) {
+        
+        Human km = new Human();
+        km.setAge(21);
+        km.setName("Kamil");
+        km.setProfession("Junior Developer");
+        km.setSex("Yaxsi Oglan");
+        km.print();
+              
+        
+    }
     
 }
